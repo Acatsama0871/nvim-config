66673b74830404198796c3c3a1c20026e635a9be
Haohang Li <haohang.li@acatsama.io>
2024-04-06 19:47:32 +0000

    add LSP

--- lua/acatsama/lazy.lua
+++ lua/acatsama/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "acatsama.plugins" },}, {
+require("lazy").setup({ { import = "acatsama.plugins" }, { import = "acatsama.plugins.lsp" }}, {
   checker = {
     enabled = true,
     notify = false,
