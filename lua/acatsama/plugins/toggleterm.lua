cbbc0b0d7ee5379620748a66218ecbf8e5e2c589
Haohang Li <haohang.li@acatsama.io>
2024-05-09 20:15:48 +0000

    config toggle term keymap

--- lua/acatsama/plugins/toggleterm.lua
+++ lua/acatsama/plugins/toggleterm.lua
@@ -8,5 +8,9 @@ return {
 			direction = "horizontal",
 			shell = "/bin/bash",
 		})
+
+		-- set keymap
+		local keymap = vim.keymap
+		keymap.set("n", "<leader>vt", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal" })
 	end,
 }
