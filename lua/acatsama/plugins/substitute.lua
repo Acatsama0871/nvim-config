31826f353daca93188e53f454c3d168b7c04ac6f
Haohang Li <hli113@stevens.edu>
2024-05-10 02:47:46 +0000

    key conflict

--- lua/acatsama/plugins/substitute.lua
+++ lua/acatsama/plugins/substitute.lua
@@ -9,9 +9,9 @@ return {
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
-		vim.keymap.set("n", "y", substitute.operator, { desc = "Substitute with motion" })
-		vim.keymap.set("n", "yy", substitute.line, { desc = "Substitute line" })
-		vim.keymap.set("n", "Y", substitute.eol, { desc = "Substitute to end of line" })
-		vim.keymap.set("x", "y", substitute.visual, { desc = "Substitute in visual mode" })
+		-- vim.keymap.set("n", "y", substitute.operator, { desc = "Substitute with motion" })
+		-- vim.keymap.set("n", "yy", substitute.line, { desc = "Substitute line" })
+		-- vim.keymap.set("n", "Y", substitute.eol, { desc = "Substitute to end of line" })
+		-- vim.keymap.set("x", "y", substitute.visual, { desc = "Substitute in visual mode" })
 	end,
 }
